f2b508ed82a21f4cbc6a50c98e56685bcb8ea64b
Hiroki Oyama <oyamahiroki14@gmail.com>
2026-05-05 09:10:47 +0900

    大幅な修正：駅の大幅追加、タブ生成、画像追加手前、ログイン機能

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-
 import { db } from "./firebase";
 import {
   collection,
@@ -16,24 +15,83 @@ import {
   onAuthStateChanged,
 } from "firebase/auth";
 
+import { storage } from "./firebase";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 
-const stations = [
-  { id: 1, name: "池袋", x: 260, y: 80 },
-  { id: 2, name: "新宿", x: 180, y: 170 },
-  { id: 3, name: "渋谷", x: 210, y: 280 },
-  { id: 4, name: "東京", x: 390, y: 230 },
-  { id: 5, name: "上野", x: 410, y: 110 },
+
+const chuoStations = [
+  { id: 1, name: "神田", x: 80, y: 80 },
+  { id: 2, name: "新宿", x: 190, y: 80 },
+  { id: 3, name: "中野", x: 300, y: 80 },
+  { id: 4, name: "高円寺", x: 410, y: 80 },
+  { id: 5, name: "荻窪", x: 520, y: 80 },
+  { id: 6, name: "吉祥寺", x: 520, y: 230, label: "bottom" },
+  { id: 7, name: "三鷹", x: 410, y: 230, label: "bottom" },
+  { id: 8, name: "東小金井", x: 300, y: 230, label: "bottom" },
+  { id: 9, name: "武蔵小金井", x: 190, y: 230, label: "bottom" },
+  { id: 10, name: "国分寺", x: 80, y: 230, label: "bottom"},
 ];
 
+const yamanoteStations = [
+  { id: 1, name: "原宿", x: 200, y: 80, label: "top"},
+  { id: 2, name: "新宿", x: 280, y: 80, label: "top" },
+  { id: 3, name: "新大久保", x: 360, y: 80, label: "top" },
+  { id: 4, name: "高田馬場", x: 400, y: 130, label: "right" },
+  { id: 5, name: "池袋", x: 400, y: 190, label: "right" },
+  { id: 6, name: "上野", x: 400, y: 250, label: "right" },
+  { id: 7, name: "秋葉原", x: 320, y: 280, label: "bottom" },
+  { id: 8, name: "東京", x: 260, y: 280, label: "bottom" },
+  { id: 9, name: "新橋", x: 200, y: 280, label: "bottom" },
+  { id: 10, name: "品川", x: 150, y: 280, label: "bottom" },
+  { id: 11, name: "目黒", x: 150, y: 220, label: "right" },
+  { id: 12, name: "恵比寿", x: 150, y: 170, label: "right" },
+  { id: 13, name: "渋谷", x: 150, y: 120, label: "right" },
+];
+
+const denentoshiStations = [
+  { id: 1, name: "長津田", x: 80, y: 80 },
+  { id: 2, name: "あざみ野", x: 200, y: 80 },
+  { id: 3, name: "たまプラーザ", x: 320, y: 80 },
+  { id: 4, name: "溝の口", x: 440, y: 80 },
+  { id: 5, name: "二子玉川", x: 440, y: 220, label: "right" },
+  { id: 6, name: "三軒茶屋", x: 320, y: 220 },
+  { id: 7, name: "池尻大橋", x: 200, y: 220 },
+  { id: 8, name: "渋谷", x: 80, y: 220 },
+  { id: 9, name: "表参道", x: 80, y: 360, label: "bottom" },
+  { id: 10, name: "永田町", x: 200, y: 360, label: "bottom" },
+  { id: 11, name: "神保町", x: 320, y: 360, label: "bottom" },
+  { id: 12, name: "大手町", x: 440, y: 360, label: "bottom" },
+];
+
+
 function App() {
+const [selectedLine, setSelectedLine] = useState("中央線");
+
+let stations = chuoStations;
+
+if (selectedLine === "山手線") {
+  stations = yamanoteStations;
+} else if (selectedLine === "田園都市線・半蔵門線") {
+  stations = denentoshiStations;
+}
+
   const [selectedStation, setSelectedStation] = useState("新宿");
   const [shopName, setShopName] = useState("");
   const [genre, setGenre] = useState("家系");
   const [rating, setRating] = useState("");
   const [memo, setMemo] = useState("");
+  const [imageFile, setImageFile] = useState(null);
   const [posts, setPosts] = useState([]);
   const [user, setUser] = useState(null); 
+
+
+
+if (selectedLine === "山手線") {
+  stations = yamanoteStations;
+} else if (selectedLine === "田園都市線・半蔵門線") {
+  stations = denentoshiStations;
+}
   
   const login = async () => {
   const provider = new GoogleAuthProvider();
@@ -90,12 +148,21 @@ useEffect(() => {
     return;
   }
 
+  let imageUrl = "";
+
+if (imageFile) {
+  const imageRef = ref(storage, `posts/${Date.now()}_${imageFile.name}`);
+  await uploadBytes(imageRef, imageFile);
+  imageUrl = await getDownloadURL(imageRef);
+}
+
   const newPost = {
     station: selectedStation,
     shopName,
     genre,
     rating: Number(rating),
     memo,
+    imageUrl,
     createdAt: new Date(),
     userId: user.uid,
     userName: user.displayName || "名前なし",
@@ -109,6 +176,7 @@ useEffect(() => {
     setGenre("家系");
     setRating("");
     setMemo("");
+    setImageFile(null);
   } catch (error) {
     console.error("投稿エラー:", error);
     alert("投稿に失敗しました");
@@ -129,6 +197,36 @@ const deletePost = async (id) => {
     <div style={{ maxWidth: "900px", margin: "30px auto", padding: "20px" }}>
       <h1>ラーメン路線図マップ</h1>
       <p>駅を選ぶと、その駅周辺のラーメン記録を見られます。</p>
+
+      <div style={{ display: "flex", gap: "8px", marginBottom: "20px" }}>
+  <button
+    onClick={() => setSelectedLine("中央線")}
+    style={{
+      background: selectedLine === "中央線" ? "#ff7043" : "#eee",
+    }}
+  >
+    中央線
+  </button>
+
+  <button
+    onClick={() => setSelectedLine("山手線")}
+    style={{
+      background: selectedLine === "山手線" ? "#ff7043" : "#eee",
+    }}
+  >
+    山手線
+  </button>
+
+  <button
+    onClick={() => setSelectedLine("田園都市線・半蔵門線")}
+    style={{
+      background:
+        selectedLine === "田園都市線・半蔵門線" ? "#ff7043" : "#eee",
+    }}
+  >
+    田園都市線・半蔵門線
+  </button>
+</div>
 
 {user ? (
   <div>
@@ -156,13 +254,38 @@ const deletePost = async (id) => {
         >
           <h2>路線図</h2>
 
-          <svg width="520" height="360" style={{ background: "#fafafa" }}>
-            <line x1="260" y1="80" x2="180" y2="170" stroke="#333" strokeWidth="4" />
-            <line x1="180" y1="170" x2="210" y2="280" stroke="#333" strokeWidth="4" />
-            <line x1="260" y1="80" x2="410" y2="110" stroke="#333" strokeWidth="4" />
-            <line x1="410" y1="110" x2="390" y2="230" stroke="#333" strokeWidth="4" />
-            <line x1="390" y1="230" x2="210" y2="280" stroke="#333" strokeWidth="4" />
-
+          <svg width="620" height="430" style={{ background: "#fafafa" }}>
+          {selectedLine === "中央線" && (
+  <>
+    {/* 上段 */}
+    <line x1="80" y1="80" x2="520" y2="80" stroke="#333" strokeWidth="4" />
+    {/* 右の縦 */}
+    <line x1="520" y1="80" x2="520" y2="230" stroke="#333" strokeWidth="4" />
+    {/* 下段 */}
+    <line x1="520" y1="230" x2="80" y2="230" stroke="#333" strokeWidth="4" />
+  </>
+)}
+{selectedLine === "山手線" && (
+  <>
+    {/* 上 */}
+    <line x1="150" y1="80" x2="400" y2="80" stroke="#333" strokeWidth="4" />
+    {/* 右 */}
+    <line x1="400" y1="80" x2="400" y2="280" stroke="#333" strokeWidth="4" />
+    {/* 下 */}
+    <line x1="400" y1="280" x2="150" y2="280" stroke="#333" strokeWidth="4" />
+    {/* 左 */}
+    <line x1="150" y1="280" x2="150" y2="80" stroke="#333" strokeWidth="4" />
+  </>
+)}
+{selectedLine === "田園都市線・半蔵門線" && (
+  <>
+    <line x1="80" y1="80" x2="440" y2="80" stroke="#333" strokeWidth="4" />
+    <line x1="440" y1="80" x2="440" y2="220" stroke="#333" strokeWidth="4" />
+    <line x1="440" y1="220" x2="80" y2="220" stroke="#333" strokeWidth="4" />
+    <line x1="80" y1="220" x2="80" y2="360" stroke="#333" strokeWidth="4" />
+    <line x1="80" y1="360" x2="440" y2="360" stroke="#333" strokeWidth="4" />
+  </>
+)}
             {stations.map((station) => (
               <g
                 key={station.id}
@@ -178,13 +301,31 @@ const deletePost = async (id) => {
                   strokeWidth="3"
                 />
                 <text
-                  x={station.x}
-                  y={station.y - 28}
-                  textAnchor="middle"
+                  x={
+                    station.label === "left"
+                      ? station.x - 30
+                      : station.label === "right"
+                      ? station.x + 30
+                      : station.x
+                  }
+                  y={
+                    station.label === "bottom"
+                      ? station.y + 36
+                      : station.label === "left" || station.label === "right"
+                      ? station.y + 5
+                      : station.y - 28
+                  }
+                  textAnchor={
+                    station.label === "left"
+                      ? "end"
+                      : station.label === "right"
+                      ? "start"
+                      : "middle"
+                  }
                   fontSize="14"
-                >
+>               
                   {station.name}
-                </text>
+</                text>
                 <text
                   x={station.x}
                   y={station.y + 5}
@@ -242,7 +383,13 @@ const deletePost = async (id) => {
               value={memo}
               onChange={(e) => setMemo(e.target.value)}
             />
-
+            {/*  戻したら画像追加できる　一枚０．３円とか
+            <input
+               type="file"
+               accept="image/*"
+               onChange={(e) => setImageFile(e.target.files[0])}
+/>
+            */}
             <button onClick={addPost} disabled={!user}>
               追加
             </button>
@@ -269,6 +416,20 @@ const deletePost = async (id) => {
                 <div>ジャンル: {post.genre}</div>
                 <div>評価: ★{post.rating}</div>
                 <div>メモ: {post.memo}</div>
+
+              {post.imageUrl && (
+               <img
+               src={post.imageUrl}
+               alt={post.shopName}
+               style={{
+                        width: "100%",
+                        maxWidth: "300px",
+                        borderRadius: "8px",
+                        marginTop: "8px",
+                     }}
+  />
+             )}
+
               </div>
             ))
           )}
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyD1Xsvl-CPgs_KTvBXgy-MFoqsMdmx8SM8",
@@ -16,3 +19,4 @@ const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const storage = getStorage(app);
